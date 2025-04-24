ae779b7004cf3fca7cda28434ca8e0c066ea2de8
Eren Yilmaz <erenyilmaz95@gmx.de>
2025-04-25 01:37:49 +0200

    Sample ranks from a given rating distribution

--- data/custom_trueskill.py
+++ data/custom_trueskill.py
@@ -1,6 +1,7 @@
 import itertools
 import math
-from typing import List, Iterable
+import random
+from typing import List, Iterable, Tuple
 
 from trueskill import TrueSkill, Rating
 
@@ -11,10 +12,25 @@ class CustomTrueSkill(TrueSkill):
         tau = sigma / 100
         super().__init__(mu=mu, sigma=sigma, beta=beta, tau=tau, draw_probability=draw_probability)
 
-    def win_probability(self, team1: Iterable[Rating], team2: Iterable[Rating]):
+    def win_probability(self, team1: Tuple[Rating], team2: Tuple[Rating]):
         # https://trueskill.org/#win-probability
         delta_mu = sum(r.mu for r in team1) - sum(r.mu for r in team2)
         sum_sigma = sum(r.sigma ** 2 for r in itertools.chain(team1, team2))
         size = len(team1) + len(team2)
         denominator = math.sqrt(size * (self.beta * self.beta) + sum_sigma)
         return self.cdf(delta_mu / denominator)
+
+    def sample_ranks(self, rating_groups: List[Tuple[Rating]]):
+        performances = [
+            sum([
+                random.normalvariate(rating.mu, rating.sigma - self.beta)
+                for rating in rating_group
+            ])
+            for rating_group in rating_groups
+        ]
+        sorted_performances = sorted(performances, reverse=True)
+        ranks = [
+            sorted_performances.index(p)
+            for p in performances
+        ]
+        return ranks
--- test/test_trueskill.py
+++ test/test_trueskill.py
@@ -32,7 +32,7 @@ class TestTrueSkill(unittest.TestCase):
     def test_playing_ffa(self):
         ts = CustomTrueSkill()
         num_players = 64
-        ratings = [(ts.create_rating(), ) for _ in range(num_players)]
+        ratings = [(ts.create_rating(),) for _ in range(num_players)]
         new_ratings = ts.rate(ratings, [i for i in range(len(ratings))])
         for i, (new_rating,) in enumerate(new_ratings):
             print(f"Place {i}: mu={new_rating.mu:.1f}, sigma={new_rating.sigma:.1f}")
@@ -41,15 +41,67 @@ class TestTrueSkill(unittest.TestCase):
         ts = CustomTrueSkill()
         num_players = 64
         num_games = 100
-        ratings = [(ts.create_rating(), ) for _ in range(num_players)]
+        ratings = [(ts.create_rating(),) for _ in range(num_players)]
         for i in range(num_games):
             results = [i for i in range(len(ratings))]
             random.shuffle(results)
             ratings = ts.rate(ratings, results)
-        for i, (new_rating,) in enumerate(ratings):
-            print(f"Player {i}: mu={new_rating.mu:.1f}, sigma={new_rating.sigma:.1f}")
+        self.print_ratings(ratings)
 
         player_1_vs_2_estimated_win_probability = ts.win_probability(ratings[0], ratings[1])
         assert 0.4 <= player_1_vs_2_estimated_win_probability <= 0.6
         print(player_1_vs_2_estimated_win_probability)
 
+    def test_playing_multiple_ffa_starting_from_uneven_ratings_but_with_equal_win_probabilities_should_go_back_to_similar_ratings(self):
+        ts = CustomTrueSkill()
+        num_players = 64
+        num_games = 100
+        initial_spread = 640
+        ratings = [(ts.create_rating(ts.mu - initial_spread / 2 + player_idx * initial_spread / num_players),)
+                   for player_idx in range(num_players)]
+
+        player_1_vs_63_estimated_win_probability = ts.win_probability(ratings[0], ratings[63])
+        assert player_1_vs_63_estimated_win_probability < 0.05
+
+        for i in range(num_games):
+            results = [i for i in range(len(ratings))]
+            random.shuffle(results)
+            ratings = ts.rate(ratings, results)
+
+        self.print_ratings(ratings)
+
+        player_1_vs_2_estimated_win_probability = ts.win_probability(ratings[0], ratings[1])
+        print(player_1_vs_2_estimated_win_probability)
+        assert 0.4 <= player_1_vs_2_estimated_win_probability <= 0.6
+
+        player_1_vs_63_estimated_win_probability = ts.win_probability(ratings[0], ratings[63])
+        print(player_1_vs_63_estimated_win_probability)
+        assert 0.4 <= player_1_vs_63_estimated_win_probability <= 0.6
+
+    def test_sampling_ranks_does_not_change_ratings_much(self):
+        ts = CustomTrueSkill()
+        num_players = 64
+        num_games = 100
+        initial_spread = 640
+        ratings = [(ts.create_rating(ts.mu - initial_spread / 2 + player_idx * initial_spread / num_players),)
+                   for player_idx in range(num_players)]
+        ratings_before = ratings.copy()
+
+        player_1_vs_63_estimated_win_probability = ts.win_probability(ratings[0], ratings[63])
+        assert player_1_vs_63_estimated_win_probability < 0.05
+
+        for i in range(num_games):
+            results = ts.sample_ranks(ratings_before)
+            ratings = ts.rate(ratings, results)
+
+        for i, ((new_rating,), (old_rating,)) in enumerate(zip(ratings, ratings_before)):
+            print(f"Player {i}: mu={new_rating.mu:.1f}, sigma={new_rating.sigma:.1f}, delta_mu={new_rating.mu - old_rating.mu:.1f}, delta_sigma={new_rating.sigma - old_rating.sigma:.1f}")
+
+        player_1_vs_63_estimated_win_probability = ts.win_probability(ratings[0], ratings[63])
+        print(player_1_vs_63_estimated_win_probability)
+        assert player_1_vs_63_estimated_win_probability < 0.05
+
+
+    def print_ratings(self, ratings):
+        for i, (new_rating,) in enumerate(ratings):
+            print(f"Player {i}: mu={new_rating.mu:.1f}, sigma={new_rating.sigma:.1f}")
